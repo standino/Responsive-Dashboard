f187f0827c8da5fceaa32da5b5cbf425280fe898
Will@Desktop <changwei.cn@gmail.com>
2014-08-29 09:06:11 +0800

    优化

--- src/js/dashboard/controllers/alerts-ctrl.js
+++ src/js/dashboard/controllers/alerts-ctrl.js
@@ -5,7 +5,7 @@ angular.module('Dashboard').controller('AlertsCtrl', ['$scope', AlertsCtrl]);
 
 function AlertsCtrl($scope) {
     $scope.alerts = [
-        { type: 'success', msg: '截至2014年08月25日，根据POP范围内来自移动端的数据显示，在东北区域内白电采销部、烟机/灶具的预测销量为100万台，整体未来销售走势良好。' }
+        { type: 'success', msg: '截至2014年08月25日，根据全国范围内来自移动端的数据显示，在东北区域内白电采销部、烟机/灶具的预测销量为100万台，整体未来销售走势良好。' }
         // { type: 'danger', msg: 'Found a bug? Create an issue with as many details as you can.' }
     ];
 
