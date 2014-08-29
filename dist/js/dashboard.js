928f22f9dc956e759c691cfa6f3ad2654d265677
Will@Desktop <changwei.cn@gmail.com>
2014-08-29 08:55:58 +0800

    初始实验angularjs

---
+++ dist/js/dashboard.js
@@ -0,0 +1,122 @@
+(function(){ 
+angular.module('Dashboard', ['ui.bootstrap', 'ui.router', 'ngCookies']);
+'use strict';
+
+/**
+ * Route configuration for the Dashboard module.
+ */
+angular.module('Dashboard').config(['$stateProvider', '$urlRouterProvider', 
+    function($stateProvider, $urlRouterProvider) {
+
+    // For unmatched routes
+    $urlRouterProvider.otherwise('/');
+
+    // Application routes
+    $stateProvider
+        .state('index', {
+            url: '/',
+            templateUrl: 'dashboard.html'
+        })
+        .state('tables', {
+            url: '/tables', 
+            templateUrl: 'tables.html'
+        });
+}]);
+
+/**
+ * Master Controller
+ */
+angular.module('Dashboard')
+    .controller('MasterCtrl', ['$scope', '$cookieStore', MasterCtrl]);
+
+function MasterCtrl($scope, $cookieStore) {
+    /**
+     * Sidebar Toggle & Cookie Control
+     *
+     */
+    var mobileView = 992;
+
+    $scope.getWidth = function() { return window.innerWidth; };
+
+    $scope.$watch($scope.getWidth, function(newValue, oldValue)
+    {
+        if(newValue >= mobileView)
+        {
+            if(angular.isDefined($cookieStore.get('toggle')))
+            {
+                if($cookieStore.get('toggle') == false)
+                {
+                    $scope.toggle = false;
+                }            
+                else
+                {
+                    $scope.toggle = true;
+                }
+            }
+            else 
+            {
+                $scope.toggle = true;
+            }
+        }
+        else
+        {
+            $scope.toggle = false;
+        }
+
+    });
+
+    $scope.toggleSidebar = function() 
+    {
+        $scope.toggle = ! $scope.toggle;
+
+        $cookieStore.put('toggle', $scope.toggle);
+    };
+
+    window.onresize = function() { $scope.$apply(); };
+}
+
+TabsDemoCtrl = function ($scope) {
+  $scope.tabs = [
+    { title:'品类分析', content:'Dynamic content 1' },
+    { title:'部门分析', content:'Dynamic content 2', disabled: true }
+  ];
+
+  $scope.alertMe = function() {
+    setTimeout(function() {
+      alert('You\'ve selected the alert tab!');
+    });
+  };
+};
+/**
+ * Alerts Controller
+ */
+angular.module('Dashboard').controller('AlertsCtrl', ['$scope', AlertsCtrl]);
+
+function AlertsCtrl($scope) {
+    $scope.alerts = [
+        { type: 'success', msg: '截至2014年08月25日，根据POP范围内来自移动端的数据显示，在东北区域内白电采销部、烟机/灶具的预测销量为100万台，整体未来销售走势良好。' }
+        // { type: 'danger', msg: 'Found a bug? Create an issue with as many details as you can.' }
+    ];
+
+    $scope.addAlert = function() {
+        $scope.alerts.push({msg: 'Another alert!'});
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+}
+/**
+ * Loading Directive
+ * @see http://tobiasahlin.com/spinkit/
+ */
+angular.module('Dashboard').directive('rdLoading', rdLoading);
+
+function rdLoading () {
+    var directive = {
+        restrict: 'AE',
+        template: '<div class="loading"><div class="double-bounce1"></div><div class="double-bounce2"></div></div>'
+    };
+    return directive;
+};
+})();
